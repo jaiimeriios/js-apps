12b76157facf4fe3c21c93c6c0a407b6fba4fded
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-05-21 18:20:28 -0700

    ad taskt to dom

--- 07 crud con local storage/app.js
+++ 07 crud con local storage/app.js
@@ -7,9 +7,8 @@ const listaActividadesUI = document.querySelector('#listaActividades');
 let arrayActividades = [];
 
 
-// funcciones
+// funciones
 const CrearTask = (actividad) => {
-
 	let item = {
 		actividad: actividad,
 		estado: false
@@ -18,9 +17,53 @@ const CrearTask = (actividad) => {
 	return item;
 }
 
-let task = CrearTask('adf');
-let otraTask = CrearTask('qwerqwer')
-console.log(task, otraTask)
+const GuardarDB = () => {
+	localStorage.setItem('task', JSON.stringify(arrayActividades))
+	PintarDB()
+}
+
+const PintarDB = () => {
+	listaActividadesUI.innerHTML = '';
+	arrayActividades = JSON.parse(localStorage.getItem('task'))
+
+	if (arrayActividades === null) {
+		arrayActividades = [];
+	} else {
+		arrayActividades.forEach(element => {
+			listaActividadesUI.innerHTML +=
+			`
+			<div class="alert alert-warning" role="alert">
+				<i class="material-icons float-left">
+					keyboard_arrow_right
+				</i>
+				<span class="font-weight-bold">${element.actividad}</span> - ${element.estado}
+				<span class="float-right">
+					<i class="material-icons">
+						done
+					</i>
+					<i class="material-icons">
+						delete_forever
+					</i>
+				</span>
+			</div>
+			`
+		})
+	}
+}
 
 
 // eventListeners
+
+formularioUI.addEventListener('submit', (e) => {
+
+	e.preventDefault();
+	let actividadUI = document.querySelector('#actividad').value;
+	CrearTask(actividadUI);
+
+	GuardarDB();
+
+	formularioUI.reset();
+})
+
+document.addEventListener('DOMContentLoaded', PintarDB)
+
