e150b3f1b2908f898129019ef7af74a898383bfb
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-05-21 17:37:28 -0700

    js arrayActividades

--- 07 crud con local storage/app.js
+++ 07 crud con local storage/app.js
@@ -1,4 +1,26 @@
 // CRUD con Local Storage
 
 
-console.log('CRUD con Local Storage')
+// variables globales
+const formularioUI = document.querySelector('#formulario');
+const listaActividadesUI = document.querySelector('#listaActividades');
+let arrayActividades = [];
+
+
+// funcciones
+const CrearTask = (actividad) => {
+
+	let item = {
+		actividad: actividad,
+		estado: false
+	}
+	arrayActividades.push(item);
+	return item;
+}
+
+let task = CrearTask('adf');
+let otraTask = CrearTask('qwerqwer')
+console.log(task, otraTask)
+
+
+// eventListeners
