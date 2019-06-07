f729afbc408daaf3f9e8b6a68ae23dc581ea6ccd
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-06-06 18:50:32 -0700

    :ballot_box_with_check: simple todo list

--- 01 simple to-do list app/app.js
+++ 01 simple to-do list app/app.js
@@ -4,30 +4,26 @@ let todoForm = document.getElementById("todoForm");
 let todoInput = document.getElementById("todoInput");
 let itemList = document.getElementById("itemList");
 
-
-todoForm.addEventListener("submit", function(e){
-
+todoForm.addEventListener("submit", function (e) {
 	e.preventDefault();
-	
-	if(todoInput.value == ""){
+	if (todoInput.value == "") {
 		inputoEmpty();
 		return false;
 	}
-
 	addItem(todoInput.value);
 });
 
-function inputoEmpty(){
+function inputoEmpty() {
 	console.log('empty');
 }
 
-function addItem(item){
+function addItem(item) {
 	let listItem = `<li>${item} <button onclick="removeItem(this)">x</button>`
 	list.insertAdjacentHTML("afterbegin", listItem)
 	todoInput.value = "";
 	todoInput.focus();
 }
 
-function removeItem(itemToDelete){
+function removeItem(itemToDelete) {
 	itemToDelete.parentElement.remove();
 }
