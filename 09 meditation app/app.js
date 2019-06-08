074202b1892aa1d19b7edbadbaf5a9f7f1e50977
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-06-08 15:45:26 -0700

    :hourglass: meditation app

--- 09 meditation app/app.js
+++ 09 meditation app/app.js
@@ -6,63 +6,16 @@ const app = () => {
 	const play = document.querySelector('.play');
 	const outline = document.querySelector('.moving-outline circle');
 	const video = document.querySelector('.vid-container video');
+
 	// sounds
-	const sounds = document.querySelectorAll('sound-picker button');
+	const sounds = document.querySelectorAll('.sound-picker button');
 	// time display
-	const timeDisplay = document.querySelector('.time-display');
-	const timeSelect = document.querySelectorAll('.time-select button');
-	// lendgth of corcle outline
-	const outlineLength = outline.getTotalLength();
-	// duration
-	let fakeDuration = 600;
-
-	outline.style.strokeDasharray = outlineLength;
-	outline.style.strokeDashoffset = outlineLength;
-
-	//play sound
-	play.addEventListener('click', () => {
-		checkPlaying(song)
-	});
-
-	// play and stop sounds
-	const checkPlaying = song => {
-		if (song.paused) {
-			song.play();
-			video.play();
-			play.src = 'svg/pause.svg'
-		} else {
-			song.pause();
-			video.pause();
-			play.src = 'svg/play.svg'
-		}
-	}
-
-	// check time and animate circle
-	song.ontimeupdate = () => {
-		let currentTime = song.currentTime;
-		let elapsed = fakeDuration;
-		let seconds = Math.floor(elapsed % 60);
-		let minutes = Math.floor(elapsed / 60);
-
-		// animate circle bar
-		let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
-		outline.style.strokeDashoffset = progress;
-
-		// nimate text
-		timeDisplay.textContent = `${minutes}:${seconds}`
-	};
+	const timeDisplay = document.querySelector('time-display');
 
 
-	// select sound
-	timeSelect.forEach(option => {
-		option.addEventListener('click', function(){
-			fakeDuration = this.getAttribute('data-time');
-			timeDisplay.textContent = `${Math.floor}`
-		})
-	})
+
 
 
 }
-
 
 app();
