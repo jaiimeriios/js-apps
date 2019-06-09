719e6bad614a767cb1223972eb87f3766b44fc87
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-06-08 17:23:53 -0700

    :hourglass: meditation app

--- 09 meditation app/app.js
+++ 09 meditation app/app.js
@@ -1,21 +1,79 @@
 // Meditation App
 
-const app = () => {
+const song = document.querySelector('.song');
+const play = document.querySelector('.play');
+const replay = document.querySelector('.replay');
+const outline = document.querySelector('.moving-outline circle');
+const video = document.querySelector('.vid-container video');
+// sounds
+const sounds = document.querySelectorAll('.sound-picker button');
+// time display
+const timeDisplay = document.querySelector('.time-display');
+// circle outline lenght
+const outlineLength = outline.getTotalLength();
+// duration
+const timeSelect = document.querySelectorAll('.time-select button');
+let fakeDuration = 600;
 
-	const song = document.querySelector('.song');
-	const play = document.querySelector('.play');
-	const outline = document.querySelector('.moving-outline circle');
-	const video = document.querySelector('.vid-container video');
+outline.style.strokeDasharray = outlineLength;
+outline.style.strokeDashoffset = outlineLength;
 
-	// sounds
-	const sounds = document.querySelectorAll('.sound-picker button');
-	// time display
-	const timeDisplay = document.querySelector('time-display');
+// play sound
+play.addEventListener("click", function () {
+	checkPlaying(song);
+});
+
+// picked different sound
+sounds.forEach(sounds => {
+	sounds.addEventListener('click', function(){
+		song.src = this.getAttribute('data-sound');
+		video.src = this.getAttribute('data-video');
+		checkPlaying(song);
+	})
+})
 
 
+// select time
+timeSelect.forEach(option => {
+	option.addEventListener("click", function () {
+		fakeDuration = this.getAttribute("data-time");
+		timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
+			fakeDuration % 60
+		)}`;
+	});
+});
+
+// play and stop sound
+const checkPlaying = song => {
+	if (song.paused) {
+		song.play();
+		video.play();
+		play.src = 'svg/pause.svg';
+	} else {
+		song.pause();
+		video.pause();
+		play.src = 'svg/play.svg';
+	}
+};
 
 
+// check time, animate circle & time text
+song.ontimeupdate = () => {
+	let currentTime = song.currentTime;
+	let elapsedTime = fakeDuration - currentTime;
+	let seconds = Math.floor(elapsedTime % 60);
+	let minutes = Math.floor(elapsedTime / 60);
+
+	let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+	outline.style.strokeDashoffset = progress;
+
+	timeDisplay.textContent = `${minutes}:${seconds}`
+
+	if(currentTime > fakeDuration) {
+		song.pause();
+		song.currentTime = 0;
+		song.src = 'svg/play.svg';
+		video.pause();
+	}
 
 }
-
-app();
