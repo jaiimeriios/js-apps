dde317c9571842ff929fa13a0940f9515c21476a
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-06-13 20:45:00 -0700

    task app

--- 10 task app con local storage/app.js
+++ 10 task app con local storage/app.js
@@ -1,3 +1,23 @@
 // Task App con Local Storage
 
+let formTask = document.querySelector('#formTask');
+let title = document.querySelector('#title');
+let description = document.querySelector('#description');
 
+formTask.addEventListener('submit', saveTask);
+
+function saveTask(e) {
+	e.preventDefault();
+	
+	const task = {
+		title,
+		description
+	}
+
+	localStorage.setItem('task', JSON.stringify(task));
+	JSON.parse(localStorage.getItem('task'));
+	console.log(localStorage.getItem('task'))
+
+
+	// console.log(e);
+}
