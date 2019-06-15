c1d3f1d1e316ef007f3816ae0d8e63a2b4949c06
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-06-14 18:37:48 -0700

    delete itemes

--- 10 task app con local storage/app.js
+++ 10 task app con local storage/app.js
@@ -27,9 +27,7 @@ function saveTask(e) {
 	getTask();
 }
 
-
 function getTask() {
-
 	let tasks = JSON.parse(localStorage.getItem('tasks'));
 	let taskView = document.querySelector('#tasks');
 
@@ -45,7 +43,7 @@ function getTask() {
 			<div class="card-body">
 				<h4>${title}</h4>
 				<p>${description}</p>
-				<a class="btn btn-danger">delete</a>
+				<a class="btn btn-danger" onClick="deleteTask('${title}')">delete</a>
 			</div>
 		</div>`
 		taskView.insertAdjacentHTML('afterbegin', listItem);
@@ -55,4 +53,15 @@ function getTask() {
 	description.value = '';
 }
 
+function deleteTask(title) {
+	let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+	for (let i = 0; i < tasks.length; i++) {
+		if(tasks[i].title == title){
+			tasks.splice(i, 1);
+		}
+	}
+}
+
 getTask();
+
