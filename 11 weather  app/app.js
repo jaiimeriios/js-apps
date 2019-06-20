c17fe5ee7092b962bf95ac1bf961fa9987c9bd82
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-06-19 19:37:03 -0700

    json()

--- 11 weather  app/app.js
+++ 11 weather  app/app.js
@@ -20,7 +20,7 @@ window.addEventListener('load', () => {
 
 			fetch(api)
 				.then(response => {
-					return response.JSON();
+					return response.json();
 				})
 				.then(data => {
 					console.log(data)
