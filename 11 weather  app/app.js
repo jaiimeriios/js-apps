aa8732b3590ebd7a63bef221011245097c14dc54
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-06-19 19:14:29 -0700

    js

--- 11 weather  app/app.js
+++ 11 weather  app/app.js
@@ -1,4 +1,19 @@
 // Weather App
+// cihivisaz@daymail.life
+// Qwer1234!
+
+window.addEventListener('load', () => {
+	let long;
+	let lat;
+
+	if (navigator.geolocation) {
+		navigator.geolocation = getCurrentPosition(position => {
+			long = position.coords.longitude;
+			lat = position.coords.latitude;
+
+			const api = `https://api.darksky.net/forecast/3ed2820bdef835d0923968060af681dd/37.8267,-122.4233`;
+		});
+	}
 
 
-console.log('asdf');
+})
