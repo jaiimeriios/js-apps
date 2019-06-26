50fbcc39085057be02f9cb764f510938ea8ec80a
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-06-25 17:58:08 -0700

    startGame();

--- 12 rock paper sissors/app.js
+++ 12 rock paper sissors/app.js
@@ -4,10 +4,26 @@ const game = () => {
 	let pScore = 0;
 	let cScore = 0;
 
+	// statr game
 	const startGame = () => {
+		const introScreen = document.querySelector('.intro');
+		const playBtn = document.querySelector('.intro button');
+		const match = document.querySelector('.match');
 
+		playBtn.addEventListener('click', () =>{
+			introScreen.classList.add('fadeOut');
+			match.classList.add('fadeIn');
+		})
+	}
+
+	// play match
+	const playMatch = () =>{
+		
 	}
 
 
-
+	// call inner functions
+	startGame();
 }
+
+game();
