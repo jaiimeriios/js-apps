1e78737820b1ca0ef8e8fcea40905ae201f07978
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-06-27 21:23:40 -0700

    js

--- 13 password generator app/app.js
+++ 13 password generator app/app.js
@@ -1,10 +1,24 @@
 // password generator app
 
+(function () {
+
+	var app =  document.querySelector('#app');
+	var inputCaracteres = document.querySelector('#numero-caracteres');
+
+	var configuracion = {
+		caracteres: '',
+		}
+	}
+
+	console.log(app, inputCaracteres)
 
 
 
-// document.querySelector('');
-// document.querySelector('');
-// document.querySelector('');
-// document.querySelector('');
-// document.querySelector('');
+	// document.querySelector('');
+	// document.querySelector('');
+	// document.querySelector('');
+
+
+
+
+}())
