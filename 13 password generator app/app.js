a6ccc0769749237a4549ad6d5f14632fa90d1173
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-06-30 14:31:03 -0700

    password objects

--- 13 password generator app/app.js
+++ 13 password generator app/app.js
@@ -2,15 +2,28 @@
 
 (function () {
 
-	var app =  document.querySelector('#app');
+	var app = document.querySelector('#app');
 	var inputCaracteres = document.querySelector('#numero-caracteres');
 
 	var configuracion = {
-		caracteres: '',
-		}
+		caracteres: parseInt(inputCaracteres.value),
+		simbolos: true,
+		numeros: true,
+		mayusculas: true,
+		minusculas: true
 	}
 
-	console.log(app, inputCaracteres)
+	var caracteres = {
+		numeros: '0 1 2 3 4 5 6 7 8 9',
+		simbolos: '~ ! @ # $ % ^ & * ( ) _ + - = [ { } ] ; : , < . ? /',
+		mayusculas: 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z',
+		minusculas: 'a b c d e f g h i j k l m n o p q r s t u v w x y z',
+	}
+
+
+
+
+	console.log(configuracion.caracteres)
 
 
 
