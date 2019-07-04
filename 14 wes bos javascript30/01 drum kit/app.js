b5d3f54aa32f199792f45c7bbdb07652abf82457
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-07-03 19:57:31 -0700

    drum kit js

--- 14 wes bos javascript30/01 drum kit/app.js
+++ 14 wes bos javascript30/01 drum kit/app.js
@@ -1,13 +1,29 @@
+const keys = document.querySelectorAll('.key');
 
-window.addEventListener('keydown', function(e){
-	// console.log(e.keyCode)
+window.addEventListener('keydown', playSound);
+
+keys.forEach(key => {
+	key.addEventListener('transitionend', removeTransition)
+})
+
+
+// functions -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+function playSound(e) {
 
 	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-	console.log(audio)
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-	if(!audio){
+	if (!audio) {
 		return;
 	}
 
+	audio.currentTime = 0;
 	audio.play();
-})
+	key.classList.add('playing');
+}
+
+function removeTransition(e) {
+	if (e.propertyName !== 'transform') return;
+	this.classList.remove('playing');
+}
