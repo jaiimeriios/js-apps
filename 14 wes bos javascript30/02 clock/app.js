ea4eb19b8b29239f02b93c0cb87a02953bffdc55
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2019-07-05 19:51:12 -0700

    clock

--- 14 wes bos javascript30/02 clock/app.js
+++ 14 wes bos javascript30/02 clock/app.js
@@ -1,24 +1,24 @@
-
 const hourHand = document.querySelector('.hour-hand')
 const minuteHand = document.querySelector('.min-hand');
 const secondHand = document.querySelector('.second-hand');
 
 function setDate() {
-
 	const now = new Date();
+
+	const hour = now.getHours();
+	const hourDegrees = ((hour / 12) * 360) + 90;
+	hourHand.style.transform = `rotate(${hourDegrees}deg)`
+
+	const minutes = now.getMinutes();
+	const minutesDegrees = ((minutes / 60) * 360) + 90;
+	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
 
 	const seconds = now.getSeconds();
 	const secondsDegrees = ((seconds / 60) * 360) + 90;
 	secondHand.style.transform = `rotate(${secondsDegrees}deg)`
 
-	const minutes = now.getMinutes();
-	const minutesDegrees =  ((minutes / 60) * 360) + 90;
-	minuteHand.style.transform = `rotate(${minutesDegrees}deg)`
-
-	const hour = now.getHours();
-	const hourDegrees =  ((hour / 60) * 360) + 90;
-	hourHand.style.transform = `rotate(${hourDegrees}deg)`
-
-
+	console.log(hour, minutes, seconds)
 }
 setInterval(setDate, 1000);
+
+setDate();
