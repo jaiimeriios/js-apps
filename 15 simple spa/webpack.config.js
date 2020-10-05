8aeb1d955e1654ca89f4993f127e9e50d629e196
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2020-10-04 19:23:09 -0700

    Configured spa with webpack & scss loader

---
+++ 15 simple spa/webpack.config.js
@@ -0,0 +1,28 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+
+    module: {
+        rules: [
+            // {
+            //     test: /.css$/i,
+            //     use: ['style-loader', 'css-loader']
+            // },
+            {
+                test: /\.s[ac]ss$/i,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    },
+
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
