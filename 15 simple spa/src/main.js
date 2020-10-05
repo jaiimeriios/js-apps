71867a57423ae89c822645f07283474dff96e92f
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2020-10-04 19:42:53 -0700

    Nav

--- 15 simple spa/src/main.js
+++ 15 simple spa/src/main.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './main.scss'
 
 console.log('asdf')
--- 15 simple spa/webpack.config.js
+++ 15 simple spa/webpack.config.js
@@ -9,10 +9,10 @@ module.exports = {
 
     module: {
         rules: [
-            // {
-            //     test: /.css$/i,
-            //     use: ['style-loader', 'css-loader']
-            // },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            },
             {
                 test: /\.s[ac]ss$/i,
                 use: ['style-loader', 'css-loader', 'sass-loader']
