ba2da1d50b5837150c7d38b44f684ed0b8fea123
Jaime Alejandro Rios <37629779+jaiimeriios@users.noreply.github.com>
2021-02-28 17:07:54 -0800

    :green_book:  todo js web app - added items to list

---
+++ 16 todo/js/main.js
@@ -0,0 +1,123 @@
+import ToDoList from './todolist.js';
+import ToDoItem from './todoitem.js';
+
+const toDoList = new ToDoList();
+
+// Launch app
+
+document.addEventListener('readystatechange', (event) => {
+    if (event.target.readyState === 'complete') {
+        initApp();
+    }
+});
+
+const initApp = () => {
+    // Add Listeners
+    const itemEntryForm = document.getElementById('itemEntryForm');
+    itemEntryForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        processSubmission();
+    });
+
+    // Procedural
+
+    // Load list object
+
+    // Refreash the page
+    refreshPage();
+};
+
+const refreshPage = () => {
+    clearListDisplay();
+    renderList();
+    clearItemEntryField();
+    setFocusItemEntry();
+};
+
+const clearListDisplay = () => {
+    const parentElement = document.getElementById('listItems');
+    deleteContents(parentElement);
+};
+
+const deleteContents = (parentElement) => {
+    let child = parentElement.lastElementChild;
+    while (child) {
+        parentElement.removeChild(child);
+        child = parentElement.lastElementChild;
+    }
+};
+
+const renderList = () => {
+    const list = toDoList.getList();
+    list.forEach((item) => {
+        buildListItem(item);
+    });
+};
+
+const buildListItem = (item) => {
+    const div = document.createElement('div');
+    div.className = 'item';
+    const check = document.createElement('input');
+    check.type = 'checkbox';
+    check.id = item.getId();
+    check.tabIndex = 0;
+
+    addClickListenerToCheckbox(check);
+
+    const label = document.createElement('label');
+    label.htmlFor = item.getId();
+    label.textContent = item.getItem();
+    div.appendChild(check);
+    div.appendChild(label);
+    const container = document.getElementById('listItems');
+    container.appendChild(div);
+};
+
+const addClickListenerToCheckbox = (checkbox) => {
+    checkbox.addEventListener('click', (event) => {
+        toDoList.removeItemFromList(checkbox.id);
+        // TODO: remove from persistent data
+
+        setTimeout(() => {
+            refreshPage();
+        }, 1000);
+    });
+};
+
+const clearItemEntryField = () => {
+    document.getElementById('newItem').value = '';
+};
+
+const setFocusItemEntry = () => {
+    document.getElementById('newItem').focus();
+};
+
+const processSubmission = () => {
+    const newEntryText = getNewEntry();
+    if (!newEntryText.length) return;
+    const nextItemId = calcNetItemId();
+    const toDoItem = createNewItem(nextItemId, newEntryText);
+    toDoList.addItemList(toDoItem);
+    refreshPage();
+};
+
+const getNewEntry = () => {
+    return document.getElementById('newItem').value.trim();
+};
+
+const calcNetItemId = () => {
+    let nextItemId = 1;
+    const list = toDoList.getList();
+    if (list.length > 0) {
+        nextItemId = list[list.length - 1].getId() + 1;
+    }
+    return nextItemId;
+};
+
+const createNewItem = (itemId, itemText) => {
+    const toDo = new ToDoItem();
+    toDo.setId(itemId);
+    toDo.setItem(itemText);
+    return toDo;
+};
